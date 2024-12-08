566abef3f7051bfa73d592d90ca59883548ee049
Ledmington <filippo.barbari@gmail.com>
2024-12-08 20:11:35 +0100

    Added command-line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,84 @@ void print_minimum(const Swarm& swarm, const size_t dimensions) {
 	std::cout << ") = " << std::scientific << swarm.minimum << std::endl;
 }
 
-int main() {
-	const int dimensions = 2;
-	const int num_particles = 100;
+void die(const std::string& msg) {
+	std::cerr << msg << std::endl;
+	std::exit(-1);
+}
+
+int main(const int argc, const char** argv) {
+	int dimensions = 2;
+	int num_particles = 100;
+	int max_iterations = 100;
+
+	for (int i = 1; i < argc; i++) {
+		std::string arg = std::string(argv[i]);
+		if (arg == "-h" || arg == "--help") {
+			std::cout << std::endl;
+			std::cout
+				<< "  Welcome to group 02 project for Advanced Methods for Scientific Computing course a.y. 2024/2025"
+				<< std::endl;
+			std::cout << "       SWARM SEARCH" << std::endl;
+			std::cout << std::endl;
+			std::cout << "Command-line arguments:" << std::endl;
+			std::cout << " -h, --help        Prints this message and exits." << std::endl;
+			std::cout << " -d, --dimensions  Sets the number of dimensions. Must be >0. Default: " << dimensions << "."
+					  << std::endl;
+			std::cout << " -p, --particles   Sets the number of particles. Must be >0. Default: " << num_particles
+					  << "." << std::endl;
+			std::cout << " -i, --iterations  Sets the number of iterations. Must be >0. Default: " << max_iterations
+					  << "." << std::endl;
+			std::cout << std::endl;
+			std::cout << "You can use it like so:" << std::endl;
+			std::cout << "  " << argv[0] << " -d " << dimensions << " -p " << num_particles << " -i " << max_iterations
+					  << std::endl;
+			std::cout << std::endl;
+			return 0;
+		} else if (arg == "-d" || arg == "--dimensions") {
+			i++;
+			if (i >= argc) {
+				die("Error: missing argument for -d, --dimensions");
+			}
+			try {
+				dimensions = std::stoi(std::string(argv[i]));
+			} catch (const std::exception&) {
+				die("Error: -d, --dimensions requires a number.");
+			}
+			if (dimensions <= 0) {
+				die("Error: -d, --dimensions must be >0");
+			}
+		} else if (arg == "-p" || arg == "--particles") {
+			i++;
+			if (i >= argc) {
+				die("Error: missing argument for -p, --particles");
+			}
+			try {
+				num_particles = std::stoi(std::string(argv[i]));
+			} catch (const std::exception&) {
+				die("Error: -p, --particles requires a number.");
+			}
+			if (num_particles <= 0) {
+				die("Error: -p, --particles must be >0");
+			}
+		} else if (arg == "-i" || arg == "--iterations") {
+			i++;
+			if (i >= argc) {
+				die("Error: missing argument for -i, --iterations");
+			}
+			try {
+				max_iterations = std::stoi(std::string(argv[i]));
+			} catch (const std::exception&) {
+				die("Error: -i, --iterations requires a number.");
+			}
+			if (max_iterations <= 0) {
+				die("Error: -i, --iterations must be >0");
+			}
+		} else {
+			std::cerr << "Unknown argument '" << arg << "'" << std::endl;
+			return -1;
+		}
+	}
+
 	std::vector<Particle> swarmParticles;
 
 	std::vector<double> lowerBound(dimensions);
@@ -33,15 +108,14 @@ int main() {
 		swarmParticles.push_back(Particle(dimensions, lowerBound, upperBound));
 	}
 
-	const double c1 = 2;
-	const double c2 = 2;
+	const double c1 = 2.0;
+	const double c2 = 2.0;
 	const double w_max = 0.9;
 	const double w_min = 0.4;
 	const double w = w_max;
 
 	Swarm swarm = Swarm(swarmParticles, lowerBound, upperBound, c1, c2, w);
 
-	const int max_iterations = 100;
 	const auto beginning = std::chrono::high_resolution_clock::now();
 
 	for (int i = 0; i < max_iterations; ++i) {
