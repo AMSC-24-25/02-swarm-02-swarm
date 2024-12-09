7c759217689ddb8b46dd243e7b15ba29cdd047c0
Ledmington <filippo.barbari@gmail.com>
2024-12-09 12:07:02 +0100

    Added assertions for correct number of dimensions

--- src/EuclideanDistance.cpp
+++ src/EuclideanDistance.cpp
@@ -1,9 +1,11 @@
 #include <cmath>
+#include <cassert>
 #include <vector>
 
 #include "EuclideanDistance.hpp"
 
 double EuclideanDistance::operator()(const std::vector<double>& position) const {
+	assert(position.size() > 0);
 	double sum = 0.0;
 	for (double x : position) {
 		sum += x * x;
--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -1,8 +1,10 @@
 #include <vector>
+#include <cassert>
 
 #include "Sphere.hpp"
 
 double Sphere::operator()(const std::vector<double>& position) const {
+	assert(position.size() > 0);
 	double sum = 0.0;
 	for (double x : position) {
 		sum += x * x;
