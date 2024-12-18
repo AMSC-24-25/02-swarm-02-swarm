010e8aac8dcb8f2a68edd22ea488c13766facec3
Luca Formaggia <lformaggia@users.noreply.github.com>
2024-12-18 14:32:52 +0100

    Update EuclideanDistance.cpp
    A comment

--- src/EuclideanDistance.cpp
+++ src/EuclideanDistance.cpp
@@ -6,6 +6,7 @@
 
 double EuclideanDistance::operator()(const std::vector<double>& position) const {
 	assert(position.size() > 0);
+	// since you have used other algorithms of the standard library, here you could have used transform_reduce
 	double sum = 0.0;
 	for (double x : position) {
 		sum += x * x;
