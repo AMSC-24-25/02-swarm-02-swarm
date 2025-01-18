6d3a69d19827311b29f860859b0f80b2bedbd257
Ledmington <ledmington.dev@gmail.com>
2025-01-18 23:37:20 +0100

    Adapted tests to GoogleTest

--- exec/test.cpp
+++ exec/test.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <cmath>
 
+#include <gtest/gtest.h>
+
 #include "Swarm.hpp"
 #include "Sphere.hpp"
 #include "EuclideanDistance.hpp"
@@ -13,7 +15,7 @@ double absolute_error(const double expected, const double actual) {
 	return std::abs(expected - actual);
 }
 
-bool test_sphere() {
+TEST(FunctionConvergence, Sphere) {
 	const int dimensions = 2;
 	const int num_particles = 100;
 	const int max_iterations = 100;
@@ -39,23 +41,15 @@ bool test_sphere() {
 		swarm.findBestFitness();
 	}
 
-	if (absolute_error(0.0, swarm.minimum) >= 1e-9) {
-		std::cout << "  Wrong minimum: expected 0.0 but was " << swarm.minimum << "." << std::endl;
-		return false;
-	}
+	EXPECT_LE(absolute_error(0.0, swarm.minimum), 1e-9);
+
 	std::vector<double> expected_minimum(dimensions, 0.0);
 	for (int i = 0; i < dimensions; i++) {
-		if (absolute_error(expected_minimum.at(i), swarm.bestGlobalPosition.at(i)) >= 1e-9) {
-			std::cout << "  Wrong coordinate at index " << i << ": expected " << expected_minimum.at(i) << " but was "
-					  << swarm.bestGlobalPosition.at(i) << "." << std::endl;
-			return false;
-		}
+		EXPECT_LE(absolute_error(expected_minimum.at(i), swarm.bestGlobalPosition.at(i)), 1e-9);
 	}
-
-	return true;
 }
 
-bool test_euclidean_distance() {
+TEST(FunctionConvergence, EuclideanDistance) {
 	const int dimensions = 2;
 	const int num_particles = 100;
 	const int max_iterations = 100;
@@ -81,23 +75,15 @@ bool test_euclidean_distance() {
 		swarm.findBestFitness();
 	}
 
-	if (absolute_error(0.0, swarm.minimum) >= 1e-9) {
-		std::cout << "  Wrong minimum: expected 0.0 but was " << swarm.minimum << "." << std::endl;
-		return false;
-	}
+	EXPECT_LE(absolute_error(0.0, swarm.minimum), 1e-9);
+
 	std::vector<double> expected_minimum(dimensions, 0.0);
 	for (int i = 0; i < dimensions; i++) {
-		if (absolute_error(expected_minimum.at(i), swarm.bestGlobalPosition.at(i)) >= 1e-9) {
-			std::cout << "  Wrong coordinate at index " << i << ": expected " << expected_minimum.at(i) << " but was "
-					  << swarm.bestGlobalPosition.at(i) << "." << std::endl;
-			return false;
-		}
+		EXPECT_LE(absolute_error(expected_minimum.at(i), swarm.bestGlobalPosition.at(i)), 1e-9);
 	}
-
-	return true;
 }
 
-bool test_rosenbrock() {
+TEST(FunctionConvergence, Rosenbrock) {
 	const int dimensions = 2;
 	const int num_particles = 100;
 	const int max_iterations = 100;
@@ -123,34 +109,13 @@ bool test_rosenbrock() {
 		swarm.findBestFitness();
 	}
 
-	if (absolute_error(0.0, swarm.minimum) >= 1e-6) {
-		std::cout << "  Wrong minimum: expected 0.0 but was " << swarm.minimum << "." << std::endl;
-		return false;
-	}
+	EXPECT_LE(absolute_error(0.0, swarm.minimum), 1e-6);
+
 	std::vector<double> expected_minimum(dimensions);
 	for (size_t i = 0; i < dimensions; i++) {
 		expected_minimum[i] = std::pow(r.a, static_cast<double>(i + 1));
 	}
 	for (int i = 0; i < dimensions; i++) {
-		if (absolute_error(expected_minimum.at(i), swarm.bestGlobalPosition.at(i)) >= 1e-3) {
-			std::cout << "  Wrong coordinate at index " << i << ": expected " << expected_minimum.at(i) << " but was "
-					  << swarm.bestGlobalPosition.at(i) << "." << std::endl;
-			return false;
-		}
+		EXPECT_LE(absolute_error(expected_minimum.at(i), swarm.bestGlobalPosition.at(i)), 1e-3);
 	}
-
-	return true;
-}
-
-int main() {
-	std::cout << "Test Sphere ..." << std::endl;
-	std::cout << (test_sphere() ? "  \033[32mOK\033[0m" : "  \033[31mFAILED\033[0m") << std::endl;
-
-	std::cout << "Test Euclidean Distance ..." << std::endl;
-	std::cout << (test_euclidean_distance() ? "  \033[32mOK\033[0m" : "  \033[31mFAILED\033[0m") << std::endl;
-
-	std::cout << "Test Rosenbrock ..." << std::endl;
-	std::cout << (test_rosenbrock() ? "  \033[32mOK\033[0m" : "  \033[31mFAILED\033[0m") << std::endl;
-
-	return 0;
 }
