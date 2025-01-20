5ab2bd3917c33d3f77e5a53737911e65ccbd82d6
Ledmington <filippo.barbari@gmail.com>
2025-01-20 23:51:22 +0100

    Refactoring rosebrock

--- include/Rosenbrock.hpp
+++ include/Rosenbrock.hpp
@@ -2,13 +2,19 @@
 #define ROSENBROCK_HPP
 
 #include <vector>
+#include <cassert>
 
 #include "ObjectiveFunction.hpp"
 
 class Rosenbrock : public ObjectiveFunction {
    public:
-	const double a = 1.0;
-	const double b = 10.0;
+	const double a;
+	const double b;
+
+	Rosenbrock(const double _a = 1.0, const double _b = 10.0) : a(_a), b(_b) {
+		assert(_a >= 0.0);
+		assert(_b >= 0.0);
+	}
 
 	virtual double operator()(const std::vector<double>& position) const override;
 };
