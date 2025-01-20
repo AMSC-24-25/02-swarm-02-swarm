f04647f081f88e43e9f079443950b9d7064bb8f7
Ledmington <filippo.barbari@gmail.com>
2025-01-20 23:53:26 +0100

    Refactoring euclidean distance

--- src/EuclideanDistance.cpp
+++ src/EuclideanDistance.cpp
@@ -1,15 +1,14 @@
 #include <cmath>
 #include <cassert>
 #include <vector>
+#include <functional>
+#include <numeric>
 
 #include "EuclideanDistance.hpp"
 
 double EuclideanDistance::operator()(const std::vector<double>& position) const {
 	assert(position.size() > 0);
-	// since you have used other algorithms of the standard library, here you could have used transform_reduce
-	double sum = 0.0;
-	for (double x : position) {
-		sum += x * x;
-	}
-	return std::sqrt(sum);
+
+	return std::sqrt(std::transform_reduce(position.begin(), position.end(), 0.0, std::plus<double>{},
+										   [](const double x) { return x * x; }));
 }
