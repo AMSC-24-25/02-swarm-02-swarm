33217849b7f419fcbac42bb1e5d25f997c99c9e3
Sterning171177 <giuseppe.starnini@mail.polimi.it>
2025-03-04 19:01:11 +0000

    creation files for Stochastic Tunnelling

---
+++ include/StochasticTunnelling.hpp
@@ -0,0 +1,22 @@
+#ifndef STOCHASTIC_TUNNELLING_HPP
+#define STOCHASTIC_TUNNELLING_HPP
+
+#include <vector>
+
+#include "ObjectiveFunction.hpp"
+
+class StochasticTunnelling{
+    private:
+
+
+    public:
+
+    StochasticTunnelling();
+
+    void iteration();
+
+};
+
+
+
+#endif // STOCHASTIC_TUNNELLING_HPP
---
+++ src/StochasticTunnelling.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <cassert>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <random>
+#include <limits>
+#include <utility>
+#include <numeric>
+#include <omp.h>
+
+#include "StochasticTunnelling.hpp"
+
+StochasticTunnelling::StochasticTunnelling(){}
+
+void StochasticTunnelling::iteration(){}
+
